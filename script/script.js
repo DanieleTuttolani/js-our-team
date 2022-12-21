d33e7d57b9ec5dfff28ea64493ab4f0e9609a29a
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-21 16:23:27 +0100

    ticket

---
+++ script/script.js
@@ -0,0 +1,66 @@
+const members = [
+    {   // 0
+        fullName: "Paolo Borghi",
+        role : "Founder & CEO",
+        picture: "<img src='img/paolo.jpg'>"
+    }
+    ,
+    {   // 1
+        fullName: "Sara Pagos",
+        role : "Programmatrice",
+        picture: "<img src='img/sara.jpg'>"
+
+    }
+    ,
+    {   //2
+        fullName: "Arturo Ringhio",
+        role : "Porta caffè",
+        picture: "<img src='img/arturo.jpg'>"
+
+    }
+    ,
+    {   // 3
+        fullName: "Eleonora Stellina",
+        role : "Segretaria bona",
+        picture: "<img src='img/eleonora.jpg'>"
+
+    }
+    ,
+    {   // 4
+        fullName: "Gennaro Adriano",
+        role : "Contadino Digitale",
+        picture: "<img src='img/gennaro.jpg'>"
+
+    }
+    ,
+    {   // 5
+        fullName: "Tai Beer",
+        role : "Traduttrice",
+        picture: "<img src='img/tai.jpg'>"
+
+    }
+]
+console.table(members)
+
+const grid = document.getElementById("grid");
+function prova (target, completeName, assignment, profile){
+    //elementi
+    const col = document.createElement("div");
+    const foto = document.createElement("div");
+    const info = document.createElement("div");
+    //classi
+    col.classList.add("col" ,"alt-card");
+    foto.classList.add("foto-wrapper");
+    info.classList.add("member-info","text-center")
+
+    foto.innerHTML = `${profile}`
+    info.innerText = `${completeName}`
+    info.innerText = `${assignment}`
+    //appendo al col i figli 
+    col.appendChild(foto);
+    col.appendChild(info);
+    //spedisco il tutto
+    target.appendChild(col);
+}
+
+prova(grid, members[0].fullName,members[0].role,members[0].picture);
