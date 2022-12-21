0a7b84a5b7bf26ae70c35e49970a08b8f9ff43b4
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-21 17:52:50 +0100

    ticket

--- script/script.js
+++ script/script.js
@@ -2,70 +2,70 @@ const members = [
     {   // 0
         fullName: "Paolo Borghi",
         role : "Founder & CEO",
-        picture: "<img src='img/paolo.jpg'>"
+        picture: "img/paolo.jpg>"
     }
     ,
     {   // 1
         fullName: "Sara Pagos",
         role : "Programmatrice",
-        picture: "<img src='img/sara.jpg'>"
+        picture: "img/sara.jpg>"
 
     }
     ,
     {   //2
         fullName: "Arturo Ringhio",
         role : "Porta caffè",
-        picture: "<img src='img/arturo.jpg'>"
+        picture: "img/arturo.jpg>"
 
     }
     ,
     {   // 3
         fullName: "Eleonora Stellina",
         role : "Segretaria bona",
-        picture: "<img src='img/eleonora.jpg'>"
+        picture: "img/eleonora.jpg>"
 
     }
     ,
     {   // 4
         fullName: "Gennaro Adriano",
         role : "Contadino Digitale",
-        picture: "<img src='img/gennaro.jpg'>"
+        picture: "img/gennaro.jpg>"
 
     }
     ,
     {   // 5
         fullName: "Tai Beer",
         role : "Traduttrice",
-        picture: "<img src='img/tai.jpg'>"
+        picture: "img/tai.jpg>"
 
     }
 ]
 console.table(members)
 
-const grid = document.getElementById("grid");
-function createCell (target, completeName, assignment, profile){
-    //elementi
-    const col = document.createElement("div");
-    const foto = document.createElement("div");
-    const info = document.createElement("div");
-    //classi
-    col.classList.add("col" ,"alt-card");
-    foto.classList.add("foto-wrapper");
-    info.classList.add("member-info","text-center")
 
-    foto.innerHTML = `${profile}`
-    info.innerHTML = `<p>${assignment}</p>`
-    info.innerHTML += `${completeName}`
-    console.log(info)
-    //appendo al col i figli 
-    col.appendChild(foto);
-    col.appendChild(info);
-    //spedisco il tutto
-    target.appendChild(col);
+const grid = document.getElementById("grid");
+
+const createCell = (target, list , num,profile,fullName, asignment ) => {
+
+    let prova = ""
+//compongo l'html
+    prova +=`
+    <div class="col alt-card">
+        <div id="fotoWrapper" class="foto-wrapper">
+            <img src="${list[num][profile]}" alt="${list[num][fullName]}">
+        </div>
+        <div class="member-info text-center">
+            <h4 id="memberName">${list[num][fullName]}</h4>
+            <p id="memberRole">${list[num][asignment]}</p>
+        </div>
+    </div>`
+    target.innerHTML=prova;
+
 }
+
 
 
 //meet the squad
 for(let i = 0; i<= members.length; i++){
-    createCell(grid, members[i].fullName,members[i].role,members[].picture);
+    createCell(grid, members[i],members[i].picture, members[i].fullName, members[i].role)
 }
